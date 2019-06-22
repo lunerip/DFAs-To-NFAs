67f17a6393747e194c823d48e304bfb6f0bff78d
Luis Enrique Neri <A01745995@itesm.mx>
2019-06-22 15:34:26 -0500

    DFA class: getDFA, getPointer, getPowerSate methods

--- NFA-to-DFA.py
+++ NFA-to-DFA.py
@@ -1,12 +1,12 @@
 from functools import reduce
 
+
 class NFA:
 
     def __init__(self, initialState):
         self.initialState = initialState
         self.states = {}
         self.powerSet = []
-
 
     def addState(self, input, state, pointingState):
         if state not in self.states:
@@ -19,19 +19,17 @@ class NFA:
                 self.states[state] = x
 
         else:
-            if len(pointingState)==0:
+            if len(pointingState) == 0:
                 self.states[state][input] = []
             else:
                 self.states[state][input].append(pointingState)
-
 
     def getDFA(self):
         self.powerSet = self.getPowerSet()
         dfa = {}
         for set in self.powerSet:
-            dfa[str(set)]={0: self.getPointer(set, 0), 1: self.getPointer(set, 1)}
+            dfa[str(set)] = {0: self.getPointer(set, 0), 1: self.getPointer(set, 1)}
         return dfa
-
 
     def getPointer(self, set, input):
         pointer = []
@@ -43,11 +41,13 @@ class NFA:
                     pointer.append(yy)
         return pointer
 
-
     def getPowerSet(self):
-        return reduce(lambda result, x: result + [subset + [x] for subset in result], self.states,[[]])
+        return reduce(lambda result, x: result + [subset + [x] for subset in result], self.states, [[]])
+
 
 def main():
+
+    # EJEMPLO
     nfa = NFA("q0")
     nfa.addState(1, "q0", "q0")
     nfa.addState(0, "q0", "q0")
@@ -55,14 +55,18 @@ def main():
     nfa.addState(1, "q1", "q2")
     nfa.addState(0, "q2", "")
     nfa.addState(1, "q2", "")
-    print(nfa.states)
 
     x = nfa.getDFA()
-    print(x)
+
+    print("Resultant NFA")
+    print("Q: ",x)
+    print("∑: {0,1}")
+    print("q: "+nfa.initialState)
+    print("∂: ")
     for i in x:
-        print("Estado "+i)
-        print("0: ",x[i][0])
-        print("1: ",x[i][1])
+        print(" Estado " + i)
+        print("  0: ", x[i][0])
+        print("  1: ", x[i][1])
         print("")
 
 
