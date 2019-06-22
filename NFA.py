4f44f79c9380a5ac17ded339a4b6e1efec02c482
Luis Enrique Neri <A01745995@itesm.mx>
2019-06-22 13:10:07 -0500

    DFA class: getDFA, getPointer, getPowerSate methods

--- NFA.py
+++ NFA.py
@@ -1,21 +1,31 @@
+from functools import reduce
+
+
 class NFA:
 
     def __init__(self, initialState):
         self.initialState = initialState
         self.states = {initialState: {0: [], 1: []}}
+        self.powerSet = []
+
 
     def addState(self, input, state, pointingState):
         self.states[state][input].append(pointingState)
 
+
     def getDFA(self):
-        self.completeDFAsStates()
+        self.powerSet = self.getPowerSet()
+        dfa = {}
+        for set in self.powerSet:
+            dfa[set]={0:self.getPointer(set, 0), 1:self.getPointer(set, 1)}
+        return dfa
+
+
+    def getPointer(self, set, input):
+        pointer = []
 
 
 
-
-    def completeDFAsStates(self):
-        for s in self.states:
-            for i in s:
-                if len(i) == 0:
-                    i.append("ø")
+    def getPowerSet(self):
+        return reduce(lambda result, x: result + [subset + [x] for subset in result], self.states,[[]])
 
